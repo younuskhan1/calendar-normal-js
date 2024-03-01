bb6215d544479f032132d89ae4d753e0cee7e3d3
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-01 11:32:30 +0600

    the testing is running

--- script.js
+++ script.js
@@ -23,58 +23,80 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 // get current date
 const date = new Date();
+console.log(date);
+console.log(date.setDate);
+console.log(date.getDate);
 
 // get current month
 let currentMonth = date.getMonth();
+console.log(currentMonth);
 
 // get current year
 let currentYear = date.getFullYear();
-
+console.log(currentYear);
 // function to render days
 function renderCalendar() {
   // get prev month current month and next month days
   date.setDate(1);
+  
   const firstDay = new Date(currentYear, currentMonth, 1);
+  console.log(firstDay);
   const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  console.log(lastDay);
   const lastDayIndex = lastDay.getDay();
+  console.log(lastDayIndex);
   const lastDayDate = lastDay.getDate();
+  console.log(lastDayDate);
   const prevLastDay = new Date(currentYear, currentMonth, 0);
+  console.log(prevLastDay);
   const prevLastDayDate = prevLastDay.getDate();
+  console.log(prevLastDayDate);
   const nextDays = 7 - lastDayIndex - 1;
+  console.log(nextDays);
 
   // update current year and month in header
+
   month.innerHTML = `${months[currentMonth]} ${currentYear}`;
 
   // update days html
+
   let days = "";
 
   // prev days html
+
   for (let x = firstDay.getDay(); x > 0; x--) {
     days += `<div class="day prev">${prevLastDayDate - x + 1}</div>`;
   }
 
   // current month days
+
   for (let i = 1; i <= lastDayDate; i++) {
+
     // check if its today then add today class
+
     if (
       i === new Date().getDate() &&
       currentMonth === new Date().getMonth() &&
       currentYear === new Date().getFullYear()
     ) {
       // if date month year matches add today
+
       days += `<div class="day today">${i}</div>`;
     } else {
       //else dont add today
+
       days += `<div class="day ">${i}</div>`;
     }
   }
 
   // next MOnth days
+
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="day next">${j}</div>`;
   }
 
   // run this function with every calendar render
+
   hideTodayBtn();
   daysContainer.innerHTML = days;
 }
@@ -82,22 +104,32 @@ function renderCalendar() {
 renderCalendar();
 
 nextBtn.addEventListener("click", () => {
+
   // increase current month by one
+
   currentMonth++;
   if (currentMonth > 11) {
+
     // if month gets greater that 11 make it 0 and increase year by one
+
     currentMonth = 0;
     currentYear++;
   }
   // rerender calendar
+
   renderCalendar();
 });
 
 // prev monyh btn
+
 prevBtn.addEventListener("click", () => {
+
   // increase by one
+
   currentMonth--;
+
   // check if let than 0 then make it 11 and deacrease year
+
   if (currentMonth < 0) {
     currentMonth = 11;
     currentYear--;
@@ -106,11 +138,16 @@ prevBtn.addEventListener("click", () => {
 });
 
 // go to today
+
 todayBtn.addEventListener("click", () => {
+
   // set month and year to current
+
   currentMonth = date.getMonth();
   currentYear = date.getFullYear();
+
   // rerender calendar
+
   renderCalendar();
 });
 
